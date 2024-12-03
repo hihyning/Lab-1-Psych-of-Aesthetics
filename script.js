3a3d0dc4f8f23ea52e7b4ca532017f39dd5b2b9c
Hyning Gan <hyninggan@gmail.com>
2024-12-03 16:42:29 -0500

    done

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ const config = {
 // Arrow Image Library
 const arrowLibrary = {
     inwardFacing: [
-        { id: 'i1', src: 'images/inward/arrow1.png', description: "Inward Arrow 1" },
-        { id: 'i2', src: 'images/inward/arrow2.png', description: "Inward Arrow 2" },
-        { id: 'i3', src: 'images/inward/arrow3.png', description: "Inward Arrow 3" },
-        { id: 'i4', src: 'images/inward/arrow4.png', description: "Inward Arrow 4" },
-        { id: 'i5', src: 'images/inward/arrow5.png', description: "Inward Arrow 5" },
-        { id: 'i6', src: 'images/inward/arrow6.png', description: "Inward Arrow 6" },
-        { id: 'i7', src: 'images/inward/arrow7.png', description: "Inward Arrow 7" },
-        { id: 'i8', src: 'images/inward/arrow8.png', description: "Inward Arrow 8" },
-        { id: 'i9', src: 'images/inward/arrow9.png', description: "Inward Arrow 9" },
-        { id: 'i10', src: 'images/inward/arrow10.png', description: "Inward Arrow 10" }
+        { id: 'i1', src: './inward/2.png', description: "Inward Arrow 1" },
+        { id: 'i2', src: './inward/3.png', description: "Inward Arrow 2" },
+        { id: 'i3', src: './inward/4.png', description: "Inward Arrow 3" },
+        { id: 'i4', src: './inward/5.png', description: "Inward Arrow 4" },
+        { id: 'i5', src: './inward/6.png', description: "Inward Arrow 5" },
+        { id: 'i6', src: './inward/7.png', description: "Inward Arrow 6" },
+        { id: 'i7', src: './inward/8.png', description: "Inward Arrow 7" },
+        { id: 'i8', src: './inward/9.png', description: "Inward Arrow 8" },
+        { id: 'i9', src: './inward/10.png', description: "Inward Arrow 9" },
+        { id: 'i10', src: './inward/11.png', description: "Inward Arrow 10" }
     ],
     outwardFacing: [
-        { id: 'o1', src: 'images/outward/arrow1.png', description: "Outward Arrow 1" },
-        { id: 'o2', src: 'images/outward/arrow2.png', description: "Outward Arrow 2" },
-        { id: 'o3', src: 'images/outward/arrow3.png', description: "Outward Arrow 3" },
-        { id: 'o4', src: 'images/outward/arrow4.png', description: "Outward Arrow 4" },
-        { id: 'o5', src: 'images/outward/arrow5.png', description: "Outward Arrow 5" },
-        { id: 'o6', src: 'images/outward/arrow6.png', description: "Outward Arrow 6" },
-        { id: 'o7', src: 'images/outward/arrow7.png', description: "Outward Arrow 7" },
-        { id: 'o8', src: 'images/outward/arrow8.png', description: "Outward Arrow 8" },
-        { id: 'o9', src: 'images/outward/arrow9.png', description: "Outward Arrow 9" },
-        { id: 'o10', src: 'images/outward/arrow10.png', description: "Outward Arrow 10" }
+        { id: 'o1', src: './outward/3.png', description: "Outward Arrow 1" },
+        { id: 'o2', src: './outward/5.png', description: "Outward Arrow 2" },
+        { id: 'o3', src: './outward/7.png', description: "Outward Arrow 3" },
+        { id: 'o4', src: './outward/9.png', description: "Outward Arrow 4" },
+        { id: 'o5', src: './outward/11.png', description: "Outward Arrow 5" },
+        { id: 'o6', src: './outward/13.png', description: "Outward Arrow 6" },
+        { id: 'o7', src: './outward/15.png', description: "Outward Arrow 7" },
+        { id: 'o8', src: './outward/17.png', description: "Outward Arrow 8" },
+        { id: 'o9', src: './outward/19.png', description: "Outward Arrow 9" },
+        { id: 'o10', src: './outward/21.png', description: "Outward Arrow 10" }
     ]
 };
 
@@ -162,6 +162,9 @@ function createResultsChart() {
                 label: 'Compositional Preferences',
                 data: [results.inwardCount, results.outwardCount],
                 backgroundColor: 'rgb(255,69,0)',
+                borderRadius: 5, // Rounded corners
+                borderWidth: 0, // No border
+                hoverBackgroundColor: 'rgba(255,69,0,0.7)' // Hover effect
             }]
         },
         options: {
@@ -169,16 +172,53 @@ function createResultsChart() {
             scales: {
                 y: {
                     beginAtZero: true,
+                    grid: {
+                        color: '#E5E5E5', // Light grid lines
+                        borderDash: [5, 5] // Dashed grid lines
+                    },
+                    ticks: {
+                        font: {
+                            family: 'Space Grotesk',
+                            size: 14
+                        },
+                        color: '#111111'
+                    },
                     title: {
                         display: true,
-                        text: 'Number of Choices'
+                        text: 'Number of Choices',
+                        font: {
+                            family: 'Space Grotesk',
+                            size: 16
+                        }
+                    }
+                },
+                x: {
+                    grid: {
+                        display: false // No vertical grid lines
+                    },
+                    ticks: {
+                        font: {
+                            family: 'Space Grotesk',
+                            size: 14
+                        },
+                        color: '#111111'
                     }
                 }
             },
             plugins: {
+                legend: {
+                    display: false // Hide legend
+                },
                 title: {
                     display: true,
-                    text: 'Preference for Inward vs. Outward Facing Compositions'
+                    text: 'Preference for Inward vs. Outward Facing Compositions',
+                    font: {
+                        family: 'Space Grotesk',
+                        size: 18,
+                        weight: 'normal'
+                    },
+                    padding: 20,
+                    color: '#111111'
                 }
             }
         }
@@ -187,7 +227,7 @@ function createResultsChart() {
     // Add percentage results text
     const percentageResults = document.getElementById('percentage-results');
     percentageResults.innerHTML = `
-        <p>Results Summary:</p>
+        <h3>Results Summary</h3>
         <p>You chose inward-facing compositions ${results.inwardPercentage}% of the time (${results.inwardCount} out of ${results.total} trials)</p>
         <p>Average reaction time: ${results.averageReactionTime} seconds</p>
     `;
