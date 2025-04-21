34a4a681dc511a7cec9e4976d62ee97a2ec88f1c
Hyning Gan <hyninggan@gmail.com>
2025-04-21 19:51:09 -0400

    push

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuration settings
+const config = {
+    // Google Apps Script Web App URL for data collection
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwcqAi9Y2vKSaJTzpM2DKv58kE2COL2TI7aOjGFZlqSKn705Xmuz5kVwGjOLZryjSsJlw/exec'
+};
+
+// Export the configuration
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = config;
+} 
